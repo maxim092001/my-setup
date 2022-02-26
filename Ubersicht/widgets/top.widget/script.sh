82ec52a3da0c116958795a2d91ce6911ad6594a1
Maxim Grankin <maximgran@gmail.com>
2022-02-26 12:55:58 +0300

    add nvim cfg

--- Ubersicht/widgets/top.widget/script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ps -u maximgran -c -o %cpu,%mem -f -m| head -6
--- nvim/init.lua
+++ nvim/init.lua
@@ -67,24 +67,12 @@ require('packer').startup(function(use)
   use { "tpope/vim-vinegar" }
 
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
 	-- Themer
-	-- use { 'EdenEast/nightfox.nvim' }
-	use {
-		"themercorp/themer.lua",
-		config = function()
-				require("themer").setup({
-						colorscheme = "everforest",
-						styles = {
-								comment = { style = 'italic' },
-								["function"] = { style = 'italic' }, 
-								functionbuiltin = { style = 'italic' }, 
-								variable = { style = 'italic' }, 
-								variableBuiltIn = { style = 'italic' }, 
-								parameter  = { style = 'italic' }, 
-						},
-				})
-		end
-}
+	use { "themercorp/themer.lua" }
+
+	-- Github themes
+  -- use { "projekt0n/github-nvim-theme" }
 
 	-- Terminal
 	use { "akinsho/toggleterm.nvim" }
@@ -104,8 +92,116 @@ require('packer').startup(function(use)
     },
     config = function() require'nvim-tree'.setup {} end
 	}
+	
+	-- Search
+	use { 'ggandor/lightspeed.nvim' }
+
+	-- Comments
+	use { 'numToStr/Comment.nvim' }
+
+	-- Git
+	use {
+  'lewis6991/gitsigns.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim'
+  },
+  tag = 'release' -- To use the latest release
+ }
+
+	-- Rust
+	use { 'simrat39/rust-tools.nvim' }
+
+
+	-- Which key
+	use { "folke/which-key.nvim" }
 
 end)
+
+-- Github themes 
+-- require('github-theme').setup()
+
+-- Which key
+require("which-key").setup()
+
+-- Git signs
+require('gitsigns').setup({
+	signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
+	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+  watch_gitdir = {
+    interval = 1000,
+    follow_files = true
+  },
+	 attach_to_untracked = true,
+   current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+   current_line_blame_opts = {
+  	virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+    ignore_whitespace = false,
+  },
+  -- current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	sign_priority = 6,
+  update_debounce = 100,
+  status_formatter = nil, -- Use default
+  max_file_length = 40000,
+  preview_config = {
+    -- Options passed to nvim_open_win
+    border = 'single',
+    style = 'minimal',
+    relative = 'cursor',
+    row = 0,
+    col = 1
+  },
+  yadm = {
+    enable = false
+  },
+  on_attach = function(bufnr)
+    local function map(mode, lhs, rhs, opts)
+        opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+        vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+    -- Actions
+    map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+    map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_unk<CR>')
+    map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+    map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+    map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+    map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+    map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+    map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+    map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+
+    -- Text object
+    map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+})
+-- Rust
+require('rust-tools').setup({})
+
+-- Comments
+require('Comment').setup()
+
+-- Remove gitblame text by default. Use :GitBlameToggle
+vim.g.gitblame_display_virtual_text = 0
 
 -- Global
 vim.opt_global.completeopt = { "menu", "noinsert", "noselect" }
@@ -133,6 +229,8 @@ require("toggleterm").setup{
 
 -- Completion
 require('custom.cmp').setup()
+
+-- This command contain error
 
 -- LSP mappings
 map("n", "gD", [[<cmd>lua vim.lsp.buf.definition()<CR>]])
@@ -177,25 +275,21 @@ metals_config.on_attach = function(client, bufnr)
 end
 
 
--- Telescope mappings
-map('n', '<leader>ff', ':Telescope find_files<cr>')
-map('n', '<leader>fg', ':Telescope live_grep<cr>')
-map('n', '<leader>fh', ':Telescope help_tags<cr>')
 
 -- Theme
 -- require('nightfox').load('nordfox')
-
 require("themer").setup({
-	colorscheme = "everforest",
-	styles = {
-			comment = { style = 'italic' },
-			["function"] = { style = 'italic' }, 
-			functionbuiltin = { style = 'italic' }, 
-			variable = { style = 'italic' }, 
-			variableBuiltIn = { style = 'italic' }, 
-			parameter  = { style = 'italic' }, 
-	},
+ 	colorscheme = "gruvbox-material-dark-medium",
+ 	styles = {
+ 			comment = { style = 'italic' },
+			["function"] = { style = 'italic' },
+ 			functionbuiltin = { style = 'italic' },
+ 			variable = { style = 'italic' },
+ 			variableBuiltIn = { style = 'italic' },
+ 			parameter  = { style = 'italic' },
+ 	},
 })
+
 -- Status line
 require("custom.statusline")
 
@@ -204,10 +298,6 @@ opt.statusline = "%!luaeval('Super_custom_status_line()')"
 -- Language servers
 require'lspconfig'.hls.setup{}
 require'lspconfig'.pyright.setup{}
-require'lspconfig'.sql.setup{}
--- require'lspconfig'.sumneko_lua.setup{}
-
-
 
 -- Dap config
 local dap = require("dap")
@@ -261,3 +351,14 @@ require('nvim-treesitter.configs').setup {
 require'telescope'.setup {
 	border = false,
 }
+require'telescope'.load_extension("themes")
+
+-- Telescope mappings
+map("n", "<leader>gr", [[<cmd>Telescope lsp_references<CR>]])
+map('n', '<leader>ff', [[<cmd>Telescope find_files<CR>]])
+map('n', '<leader>fg', [[<cmd>Telescope live_grep<CR>]])
+map('n', '<leader>fh', [[<cmd>Telescope help_tags<CR>]])
+
+
+-- NvimTree mappings
+map('n', "<leader>nf", [[<cmd>NvimTreeFindFile<CR>]])
